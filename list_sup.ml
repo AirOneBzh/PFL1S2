8c596925acb9dee797cd4c1f6fe558e4db4c8917
lce987 <lce987@live.fr>
2017-04-11 18:44:42 +0200

    Update list_sup.ml

--- list_sup.ml
+++ list_sup.ml
@@ -1,5 +1,6 @@
 
 let selectionne inf l = match l with 
-	[] -> failwith "liste vide" 
-  	|[x] -> x 
-    |x::y::[] -> if inf x y then x else y;;
+    [] -> failwith "liste vide" 
+    |[x] -> x 
+    |x::y::[] -> if inf x y then x else y
+    |x::y::r -> if inf x y then 
