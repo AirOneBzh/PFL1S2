c96784621f5355d36d8f3e32769bc2d5084addef
lce987 <lce987@live.fr>
2017-04-11 18:45:06 +0200

    Update list_sup.ml

--- list_sup.ml
+++ list_sup.ml
@@ -1,6 +1,1 @@
-
-let selectionne inf l = match l with 
-    [] -> failwith "liste vide" 
-    |[x] -> x 
-    |x::y::[] -> if inf x y then x else y
-    |x::y::r -> if inf x y then 
+let rec selectionne inf l = match l with [] -> failwith "liste vide" |[x] -> x |x::y::[] -> if inf x y then x else |x::y::r -> if inf x y then selectionne inf (x::r) else selectionne inf (y::r);;
