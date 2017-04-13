fb51e39b9fb62a2125eef1495ad5b22f333774f8
lce987 <lce987@live.fr>
2017-04-13 23:48:32 +0200

    Add files via upload

--- list_sup.ml
+++ list_sup.ml
@@ -1,24 +1,13 @@
-##Tri par sélection du minimum
-let rec selectionne inf l = match l with [] -> failwith "liste vide" |[x] -> x |x::y::[] -> if inf x y then x else y |x::y::r -> if inf x y then selectionne inf (x::r) else selectionne inf (y::r);;
-                
+let rec min_list inf l = match l with [] -> failwith "liste vide" |[x] -> x |x::y::[] -> if inf x y then x else y |x::y::r -> if inf x y then min_list inf (x::r) else min_list inf (y::r);;
+
 let rec supprime x l = match l with [] -> l |[n] -> if n==x then [] else [] |n::r -> if n==x then r else n::(supprime x r);;
-        
-let rec tri_selection_min inf l = match l with [] -> [] |[x] -> [x] |x::r -> (selectionne inf l)::(tri_selection_min inf (supprime (selectionne inf l) l));;
-        
-##Tri par partition-fusion
-let rec partitionne_bis l i j  = match l with [] -> (i, j) |x::[] -> (x::i, j) |x::y::r -> partitionne_bis r (x::i) (y::j);;
-   
 
-let partitionne l = partitionne_bis l [] [];; 
+let rec tri_selection_min inf l = match l with [] -> [] |[x] -> [x] |x::r -> (min_list inf l)::(tri_selection_min inf (supprime (min_list inf l) l));;
 
-# Fonction pour inverser une liste nous permet de gagner du temps en réutilisant partitionne qui séparait correctement la liste en deux liste inverses à celles voulues
+let rec partitionne l i j  = match l with [] -> (i, j) |x::[] -> (x::i, j) |x::y::r -> partitionne r (x::i) (y::j);;
 
-let rec reverse_bis l lr = match l with [] -> lr | x::r -> reverse_bis r (x::lr);;
+let rec reverse l lr = match l with [] -> lr | x::r -> reverse r (x::lr);;
 
-let reverse l= reverse_bis l [];;
+let rec fusionne inf l1 l2 l = match (l1,l2) with ([],_) -> (reverse l2 []) @ l | (_,[]) -> (reverse l1 []) @ l | (x1::r1,x2::r2) -> if inf x1 x2 then fusionne inf r1 l2 (x1::l) else fusionne inf l1 r2 (x2::l);;
 
-let rec fusionne_bis inf l1 l2 l = match (l1,l2) with ([],_) -> (reverse l2) @ l | (_,[]) -> (reverse l1) @ l | (x1::r1,x2::r2) -> if inf x1 x2 then fusionne_bis inf r1 l2 (x1::l) else fusionne_bis inf l1 r2 (x2::l);;
-
-let fusionne inf l1 l2 = reverse (fusionne_bis inf l1 l2 []);;
-
-let tri_partition_fusion inf l = let (l1,l2) = partitionne l in fusionne inf ( tri_selection_min inf l1 ) ( tri_selection_min inf l2);;
+let tri inf l = let (l1,l2) = partitionne l [] [] in reverse ( fusionne inf ( tri_selection_min inf l1 ) ( tri_selection_min inf l2) [] ) [];;
---
+++ list_sup.mli
@@ -0,0 +1,60 @@
+(* list_sup.mli *)
+
+      (************************************)
+      (*                                  *)
+      (* Specification du module list_sup *)
+      (*                                  *)
+      (************************************)
+
+(*************************************************)
+
+val min_list : ('a -> 'a -> bool) -> 'a list -> 'a
+
+(* ( min_list inf l ) permet de trouver le minimum
+    d'une liste l suivant l'ordre établi par inf *)
+
+(*************************************************)
+
+val supprime : 'a -> 'a list -> 'a list
+
+(* (supprime x l ) supprime le premier élément
+            x de la liste l                 *)
+
+(****************************************************************)
+
+val tri_selection_min : ('a -> 'a -> bool) -> 'a list -> 'a list
+
+(* (tri_selection_min inf l ) retourne une liste triée
+       avec les même éléments que la liste l *)
+
+(****************************************************************)
+
+val partitionne : 'a list -> 'a list -> 'a list -> 'a list * 'a list
+
+(* ( partitionne l i j ) sépare en deux listes la liste l,
+        i et j sont les variables qui porteront les listes séparés
+        durant les différents appels récursifs               *)
+
+(****************************************************************)
+
+val reverse : 'a list -> 'a list -> 'a list
+
+(* ( reverse l lr )  retourne la liste inversée de l,
+      durant les appels récursifs les premières valeurs de l
+      s’ajouteront au début de lr ce qui permet de retourner la liste  *)
+
+(****************************************************************)
+
+val fusionne : ('a -> 'a -> bool) -> 'a list -> 'a list -> 'a list -> 'a list
+
+(* ( fusionne inf l1 l2 l ) compare le premier élément de chaque
+              liste l1 l2 puisqu'elles sont triées afin d'ajouter
+              celui qui ressort de la comparaison inf dans la liste l   *)
+
+(****************************************************************)
+
+val tri : ('a -> 'a -> bool) -> 'a list -> 'a list
+
+(* ( tri inf l ) permet d'appeler tour à tour les fonctions
+        précédentes dans le but de trier rapidement une liste
+        par la méthode de partition fusion               *)
